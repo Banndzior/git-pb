259c9ea61f07f7a42ca4460c6b808a0e7260305e
martyna <martyna.kondraciuk333@gmail.com>
2020-10-25 10:41:20 +0100

    Moje zmiany

---
+++ markon96/Zadanie 1.js
@@ -0,0 +1,17 @@
+// ARRAY: [1,6,23,8,4,8,3,7]
+//	Create a function that returns the sum of all elements passed in array as parameter. Function (array)=>number ( Utwórz funkcję, która zwraca sumę wszystkich elementów umieszczonych w tablicy jako parametr)
+
+// ARRAY : [1,6,23,8,4,8,3,7];
+// function showSum
+// let sum = [1,6,23,8,4,8,3,7];
+// console.log.length("Sum equels =" + sum);
+
+arr = [1, 6, 23, 8, 4, 8, 3, 7];
+// 2) Create a function that returns sum of first and last elements of given array.
+
+function showSum(x, y) 
+let x = [0];
+let y = [7];
+y = x + y;
+let sum = x + y;
+console.log("Value is:" + sum);
